eeee20349a925990068fd041325bb90d8753bda4
Techprenuer1 <125614142+Techprenuer1@users.noreply.github.com>
2023-04-26 15:50:53 +0000

    "need help adding clock"

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,10 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "../styles/index.css"
 
 //import your own components
 import Home from "./component/home.jsx";
-
+let counter = 0
+setInterval(function() {
+    ReactDOM.render(<Home counter={counter} />, document.querySelector("#app"));
+    counter++
+}, 1000);
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
